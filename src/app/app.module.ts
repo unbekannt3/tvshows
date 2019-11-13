399d261a1719aee00864e63282eecd654f50f8fc
Henning Pohl <me@unbekannt3.eu>
2019-11-13 12:56:34 +0100

    Input done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { TVShowService } from './services/tvshow.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [TVShowService],
   bootstrap: [AppComponent]
--- src/app/components/add-show/add-show.component.ts
+++ src/app/components/add-show/add-show.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TVShowService } from '../../services/tvshow.service';
+import { TVShow } from '../../model/tvshow';
 
 @Component({
   selector: 'app-add-show',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddShowComponent implements OnInit {
 
-  constructor() { }
+  public tempShow: TVShow = new TVShow(null, null);
+
+  constructor(
+    public tvShowService: TVShowService
+  ) { }
+
+  add() {
+    this.tvShowService.addShow(new TVShow(this.tempShow.id, this.tempShow.name));
+    this.tempShow.id = null;
+    this.tempShow.name = null;
+  }
 
   ngOnInit() {
   }
